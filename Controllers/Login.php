41c17cf38c7650f605e71c11814c3f9a73155500
Javier Semblantes <jesemblantes1@utpl.edu.ec>
2022-01-22 16:30:27 -0500

    Configuración de acceso al panel de control desde login

--- Controllers/Login.php
+++ Controllers/Login.php
@@ -21,6 +21,36 @@
 			$this->views->getView($this,"login",$data);
 		}
 
+		public function loginUser(){
+			//dep($_POST);
+			if($_POST){
+				if(empty($_POST['txtEmail']) || empty($_POST['txtPassword'])){
+					$arrResponse = array('status' => false, 'msg' => 'Error de datos' );
+				}else{
+					$strUsuario  =  strtolower(strClean($_POST['txtEmail']));
+					$strPassword = hash("SHA256",$_POST['txtPassword']);
+					$requestUser = $this->model->loginUser($strUsuario, $strPassword);
+					if(empty($requestUser)){
+						$arrResponse = array('status' => false, 'msg' => 'El usuario o la contraseña es incorrecto.' ); 
+					}else{
+						$arrData = $requestUser;
+						if($arrData['status'] == 1){
+							$_SESSION['idUser'] = $arrData['idpersona'];
+							$_SESSION['login'] = true;
+
+							$arrData = $this->model->sessionLogin($_SESSION['idUser']);
+							sessionUser($_SESSION['idUser']);							
+							$arrResponse = array('status' => true, 'msg' => 'ok');
+						}else{
+							$arrResponse = array('status' => false, 'msg' => 'Usuario inactivo.');
+						}
+					}
+				}
+				echo json_encode($arrResponse,JSON_UNESCAPED_UNICODE);
+			}
+			die();
+		}
+
 
 	}
  ?>
