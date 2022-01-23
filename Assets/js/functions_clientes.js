a89487bd05414e4edf50f7bbeee0963d478fd3ac
Andres Mendoza <aamendoza8@utpl.edu.ec>
2022-01-23 13:41:08 -0500

    Envio de datos de formulario de Clientes por Ajax

--- Assets/js/functions_clientes.js
+++ Assets/js/functions_clientes.js
@@ -1,1 +1,136 @@
 // clientes
+let tableClientes;
+let rowTable = "";
+let divLoading = document.querySelector("#divLoading");
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		tableClientes = $("#tableClientes").dataTable({
+			aProcessing: true,
+			aServerSide: true,
+			language: {
+				url: "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json",
+			},
+			ajax: {
+				url: " " + base_url + "/Clientes/getClientes",
+				dataSrc: "",
+			},
+			columns: [
+				{ data: "idpersona" },
+				{ data: "identificacion" },
+				{ data: "nombres" },
+				{ data: "apellidos" },
+				{ data: "email_user" },
+				{ data: "telefono" },
+				{ data: "options" },
+			],
+			dom: "lBfrtip",
+			buttons: [
+				{
+					extend: "copyHtml5",
+					text: "<i class='far fa-copy'></i> Copiar",
+					titleAttr: "Copiar",
+					className: "btn btn-secondary",
+				},
+				{
+					extend: "excelHtml5",
+					text: "<i class='fas fa-file-excel'></i> Excel",
+					titleAttr: "Esportar a Excel",
+					className: "btn btn-success",
+				},
+				{
+					extend: "pdfHtml5",
+					text: "<i class='fas fa-file-pdf'></i> PDF",
+					titleAttr: "Esportar a PDF",
+					className: "btn btn-danger",
+				},
+				{
+					extend: "csvHtml5",
+					text: "<i class='fas fa-file-csv'></i> CSV",
+					titleAttr: "Esportar a CSV",
+					className: "btn btn-info",
+				},
+			],
+			resonsieve: "true",
+			bDestroy: true,
+			iDisplayLength: 10,
+			order: [[0, "desc"]],
+		});
+
+		if (document.querySelector("#formCliente")) {
+			let formCliente = document.querySelector("#formCliente");
+			formCliente.onsubmit = function (e) {
+				e.preventDefault();
+				let strIdentificacion =
+					document.querySelector("#txtIdentificacion").value;
+				let strNombre = document.querySelector("#txtNombre").value;
+				let strApellido = document.querySelector("#txtApellido").value;
+				let strEmail = document.querySelector("#txtEmail").value;
+				let intTelefono = document.querySelector("#txtTelefono").value;
+				let strNit = document.querySelector("#txtNit").value;
+				let strNomFical = document.querySelector("#txtNombreFiscal").value;
+				let strDirFiscal = document.querySelector("#txtDirFiscal").value;
+				let strPassword = document.querySelector("#txtPassword").value;
+
+				if (
+					strIdentificacion == "" ||
+					strApellido == "" ||
+					strNombre == "" ||
+					strEmail == "" ||
+					intTelefono == "" ||
+					strNit == "" ||
+					strDirFiscal == "" ||
+					strNomFical == ""
+				) {
+					swal("Atención", "Todos los campos son obligatorios.", "error");
+					return false;
+				}
+
+				let elementsValid = document.getElementsByClassName("valid");
+				for (let i = 0; i < elementsValid.length; i++) {
+					if (elementsValid[i].classList.contains("is-invalid")) {
+						swal(
+							"Atención",
+							"Por favor verifique los campos en rojo.",
+							"error"
+						);
+						return false;
+					}
+				}
+				divLoading.style.display = "flex";
+				let request = window.XMLHttpRequest
+					? new XMLHttpRequest()
+					: new ActiveXObject("Microsoft.XMLHTTP");
+				let ajaxUrl = base_url + "/Clientes/setCliente";
+				let formData = new FormData(formCliente);
+				request.open("POST", ajaxUrl, true);
+				request.send(formData);
+				request.onreadystatechange = function () {
+					if (request.readyState == 4 && request.status == 200) {
+						let objData = JSON.parse(request.responseText);
+						if (objData.status) {
+							if (rowTable == "") {
+								tableClientes.api().ajax.reload();
+							} else {
+								rowTable.cells[1].textContent = strIdentificacion;
+								rowTable.cells[2].textContent = strNombre;
+								rowTable.cells[3].textContent = strApellido;
+								rowTable.cells[4].textContent = strEmail;
+								rowTable.cells[5].textContent = intTelefono;
+								rowTable = "";
+							}
+							$("#modalFormCliente").modal("hide");
+							formCliente.reset();
+							swal("Usuarios", objData.msg, "success");
+						} else {
+							swal("Error", objData.msg, "error");
+						}
+					}
+					divLoading.style.display = "none";
+					return false;
+				};
+			};
+		}
+	},
+	false
+);
