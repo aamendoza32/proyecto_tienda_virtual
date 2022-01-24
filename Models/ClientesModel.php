4c245f92661ca11a7f67a5e69eb0afd34a9af354
Andres Mendoza <aamendoza8@utpl.edu.ec>
2022-01-23 19:11:49 -0500

    Programación de métodos Eliminar Clientes

--- Models/ClientesModel.php
+++ Models/ClientesModel.php
@@ -129,6 +129,14 @@ class ClientesModel extends Mysql
 		return $request;
 	}
 
+	public function deleteCliente(int $intIdpersona)
+	{
+		$this->intIdUsuario = $intIdpersona;
+		$sql = "UPDATE persona SET status = ? WHERE idpersona = $this->intIdUsuario ";
+		$arrData = array(0);
+		$request = $this->update($sql,$arrData);
+		return $request;
+	}
 
 
 }
