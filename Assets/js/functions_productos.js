9615aa0ef8a514dcd024851a2a0034ddb65ce79a
Kleber Pazmiño <kapazmino1@utpl.edu.ec>
2022-01-24 05:47:29 -0500

    Configuración de listado de productos

--- Assets/js/functions_productos.js
+++ Assets/js/functions_productos.js
@@ -10,6 +10,114 @@ $(document).on("focusin", function (e) {
 	}
 });
 
+window.addEventListener(
+	"load",
+	function () {
+		if (document.querySelector("#formProductos")) {
+			let formProductos = document.querySelector("#formProductos");
+			formProductos.onsubmit = function (e) {
+				e.preventDefault();
+				let strNombre = document.querySelector("#txtNombre").value;
+				let intCodigo = document.querySelector("#txtCodigo").value;
+				let strPrecio = document.querySelector("#txtPrecio").value;
+				let intStock = document.querySelector("#txtStock").value;
+				let intStatus = document.querySelector("#listStatus").value;
+				if (
+					strNombre == "" ||
+					intCodigo == "" ||
+					strPrecio == "" ||
+					intStock == ""
+				) {
+					swal("Atención", "Todos los campos son obligatorios.", "error");
+					return false;
+				}
+				if (intCodigo.length < 5) {
+					swal(
+						"Atención",
+						"El código debe ser mayor que 5 dígitos.",
+						"error"
+					);
+					return false;
+				}
+				divLoading.style.display = "flex";
+				tinyMCE.triggerSave();
+				let request = window.XMLHttpRequest
+					? new XMLHttpRequest()
+					: new ActiveXObject("Microsoft.XMLHTTP");
+				let ajaxUrl = base_url + "/Productos/setProducto";
+				let formData = new FormData(formProductos);
+				request.open("POST", ajaxUrl, true);
+				request.send(formData);
+				request.onreadystatechange = function () {
+					if (request.readyState == 4 && request.status == 200) {
+						let objData = JSON.parse(request.responseText);
+						if (objData.status) {
+							swal("", objData.msg, "success");
+							document.querySelector("#idProducto").value =
+								objData.idproducto;
+							document
+								.querySelector("#containerGallery")
+								.classList.remove("notblock");
+
+							if (rowTable == "") {
+								tableProductos.api().ajax.reload();
+							} else {
+								htmlStatus =
+									intStatus == 1
+										? '<span class="badge badge-success">Activo</span>'
+										: '<span class="badge badge-danger">Inactivo</span>';
+								rowTable.cells[1].textContent = intCodigo;
+								rowTable.cells[2].textContent = strNombre;
+								rowTable.cells[3].textContent = intStock;
+								rowTable.cells[4].textContent = smony + strPrecio;
+								rowTable.cells[5].innerHTML = htmlStatus;
+								rowTable = "";
+							}
+						} else {
+							swal("Error", objData.msg, "error");
+						}
+					}
+					divLoading.style.display = "none";
+					return false;
+				};
+			};
+		}
+
+		if (document.querySelector(".btnAddImage")) {
+			let btnAddImage = document.querySelector(".btnAddImage");
+			btnAddImage.onclick = function (e) {
+				let key = Date.now();
+				let newElement = document.createElement("div");
+				newElement.id = "div" + key;
+				newElement.innerHTML = `
+            <div class="prevImage"></div>
+            <input type="file" name="foto" id="img${key}" class="inputUploadfile">
+            <label for="img${key}" class="btnUploadfile"><i class="fas fa-upload "></i></label>
+            <button class="btnDeleteImage notblock" type="button" onclick="fntDelItem('#div${key}')"><i class="fas fa-trash-alt"></i></button>`;
+				document.querySelector("#containerImages").appendChild(newElement);
+				document.querySelector("#div" + key + " .btnUploadfile").click();
+				fntInputFile();
+			};
+		}
+
+		fntInputFile();
+		fntCategorias();
+	},
+	false
+);
+
+if (document.querySelector("#txtCodigo")) {
+	let inputCodigo = document.querySelector("#txtCodigo");
+	inputCodigo.onkeyup = function () {
+		if (inputCodigo.value.length >= 5) {
+			document.querySelector("#divBarCode").classList.remove("notblock");
+			fntBarcode();
+		} else {
+			document.querySelector("#divBarCode").classList.add("notblock");
+		}
+	};
+}
+
 tinymce.init({
 	selector: "#txtDescripcion",
 	width: "100%",
--- Controllers/Productos.php
+++ Controllers/Productos.php
@@ -23,4 +23,36 @@ class Productos extends Controllers
         $data['page_functions_js'] = "functions_productos.js";
         $this->views->getView($this, "productos", $data);
     }
+
+    public function getProductos()
+    {
+        if ($_SESSION['permisosMod']['r']) {
+            $arrData = $this->model->selectProductos();
+            for ($i = 0; $i < count($arrData); $i++) {
+                $btnView = '';
+                $btnEdit = '';
+                $btnDelete = '';
+
+                if ($arrData[$i]['status'] == 1) {
+                    $arrData[$i]['status'] = '<span class="badge badge-success">Activo</span>';
+                } else {
+                    $arrData[$i]['status'] = '<span class="badge badge-danger">Inactivo</span>';
+                }
+
+                $arrData[$i]['precio'] = SMONEY . ' ' . formatMoney($arrData[$i]['precio']);
+                if ($_SESSION['permisosMod']['r']) {
+                    $btnView = '<button class="btn btn-info btn-sm" onClick="fntViewInfo(' . $arrData[$i]['idproducto'] . ')" title="Ver producto"><i class="far fa-eye"></i></button>';
+                }
+                if ($_SESSION['permisosMod']['u']) {
+                    $btnEdit = '<button class="btn btn-primary  btn-sm" onClick="fntEditInfo(this,' . $arrData[$i]['idproducto'] . ')" title="Editar producto"><i class="fas fa-pencil-alt"></i></button>';
+                }
+                if ($_SESSION['permisosMod']['d']) {
+                    $btnDelete = '<button class="btn btn-danger btn-sm" onClick="fntDelInfo(' . $arrData[$i]['idproducto'] . ')" title="Eliminar producto"><i class="far fa-trash-alt"></i></button>';
+                }
+                $arrData[$i]['options'] = '<div class="text-center">' . $btnView . ' ' . $btnEdit . ' ' . $btnDelete . '</div>';
+            }
+            echo json_encode($arrData, JSON_UNESCAPED_UNICODE);
+        }
+        die();
+    }
 }
